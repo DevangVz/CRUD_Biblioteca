3694e6a2102c0d452fd02453803c3a0f4a13d233
DevangVz <devang.vazquez@gmail.com>
2016-06-03 17:16:19 -0500

    Update dataLibrary.sql

--- SQL/dataLibrary.sql
+++ SQL/dataLibrary.sql
@@ -41,23 +41,45 @@ INSERT INTO Publisher (pName,countrycode,address,ZC) VALUES('Editores Mexicanos'
 INSERT INTO Publisher (pName,countrycode,address,ZC) VALUES('Porrua', 'MEX','asdlñkjasd','1234567890');
 
 INSERT INTO Book (title,countrycode,ISBN,genreID,pages) VALUES('Cien años de soledad','MEX','9788423451839',1,456);
+INSERT INTO Book (title,countrycode,ISBN,genreID,pages) VALUES('QWER','USA','123',2,2);
 INSERT INTO Book (title,countrycode,ISBN,genreID,pages) VALUES('QWERTY','MEX','9788420451839',1,300);
 INSERT INTO Book (title,countrycode,ISBN,genreID,pages) VALUES('ADFG','MEX','9788420471849',1,556);
-INSERT INTO Book (title,countrycode,ISBN,genreID,pages) VALUES('ZXCVBB','MEX','9788420474839',1,876);
+INSERT INTO Book (title,countrycode,ISBN,genreID,pages) VALUES('ZXCVBB','MEX','9756420479839',1,876);
 
-INSERT INTO Reservation (clientID,bookID,borrowedDate,returnDate) VALUES(1,1,STR_TO_DATE('2-12-2012', '%m-%d-%Y'),STR_TO_DATE('2-13-2012', '%m-%d-%Y'));
-INSERT INTO Reservation (clientID,bookID,borrowedDate,returnDate) VALUES(1,2,STR_TO_DATE('2-12-2012', '%m-%d-%Y'),STR_TO_DATE('2-13-2012', '%m-%d-%Y'));
-INSERT INTO Reservation (clientID,bookID,borrowedDate,returnDate) VALUES(2,1,STR_TO_DATE('2-12-2012', '%m-%d-%Y'),STR_TO_DATE('2-13-2012', '%m-%d-%Y'));
-INSERT INTO Reservation (clientID,bookID,borrowedDate,returnDate) VALUES(1,2,STR_TO_DATE('2-12-2012', '%m-%d-%Y'),STR_TO_DATE('2-13-2012', '%m-%d-%Y'));
+INSERT INTO Reservation (userID,bookID,borrowedDate,returnDate) VALUES(1,1,STR_TO_DATE('2-12-2012', '%m-%d-%Y'),STR_TO_DATE('2-13-2012', '%m-%d-%Y'));
+INSERT INTO Reservation (userID,bookID,borrowedDate,returnDate) VALUES(1,2,STR_TO_DATE('2-12-2012', '%m-%d-%Y'),STR_TO_DATE('2-13-2012', '%m-%d-%Y'));
+INSERT INTO Reservation (userID,bookID,borrowedDate,returnDate) VALUES(2,1,STR_TO_DATE('2-12-2012', '%m-%d-%Y'),STR_TO_DATE('2-13-2012', '%m-%d-%Y'));
+INSERT INTO Reservation (userID,bookID,borrowedDate,returnDate) VALUES(1,2,STR_TO_DATE('2-12-2012', '%m-%d-%Y'),STR_TO_DATE('2-13-2012', '%m-%d-%Y'));
+INSERT INTO Reservation (userID,bookID,borrowedDate,returnDate) VALUES(3,1,STR_TO_DATE('2-12-2012', '%m-%d-%Y'),STR_TO_DATE('2-13-2012', '%m-%d-%Y'));
 
 INSERT INTO AuthorBook (bookID, authorID) VALUES (1,3);
 INSERT INTO AuthorBook (bookID, authorID) VALUES (1,4);
 INSERT INTO AuthorBook (bookID, authorID) VALUES (2,4);
 INSERT INTO AuthorBook (bookID, authorID) VALUES (4,3);
+INSERT INTO AuthorBook (bookID, authorID) VALUES (3,2);
+INSERT INTO AuthorBook (bookID, authorID) VALUES (5,1);
+
 
 INSERT INTO PublisherBook (bookID, publisherID, numberEdition,publishDate) VALUES (1,2,1,STR_TO_DATE('2-12-2012', '%m-%d-%Y'));
 INSERT INTO PublisherBook (bookID, publisherID, numberEdition,publishDate) VALUES(1,3,2,STR_TO_DATE('2-12-2012', '%m-%d-%Y'));
 INSERT INTO PublisherBook (bookID, publisherID, numberEdition,publishDate) VALUES(3,1,4,STR_TO_DATE('2-12-2012', '%m-%d-%Y'));
-INSERT INTO PublisherBook (bookID, publisherID, numberEdition,publishDate) VALUES(2,3,1,STR_TO_DATE('2-12-2012', '%m-%d-%Y'));
+INSERT INTO PublisherBook (bookID, publisherID, numberEdition,publishDate) VALUES(2,3,2,STR_TO_DATE('2-12-2012', '%m-%d-%Y'));
 INSERT INTO PublisherBook (bookID, publisherID, numberEdition,publishDate) VALUES(1,3,1,STR_TO_DATE('2-12-2012', '%m-%d-%Y'));
-INSERT INTO PublisherBook (bookID, publisherID, numberEdition,publishDate) VALUES(1,4,1,STR_TO_DATE('2-12-2012', '%m-%d-%Y'));
+INSERT INTO PublisherBook (bookID, publisherID, numberEdition,publishDate) VALUES(1,4,3,STR_TO_DATE('2-12-2012', '%m-%d-%Y'));
+INSERT INTO AuthorBook (bookID, authorID) VALUES (LAST_INSERT_ID(),5);
+
+INSERT INTO AuthorBook (bookID, authorID) VALUES (LAST_INSERT_ID(),5);
+
+SELECT distinct LAST_INSERT_ID() FROM book ;
+
+(SELECT distinct LAST_INSERT_ID() FROM book);
+
+delete from book where id = 13;
+
+
+select distinct LAST_INSERT_ID();
+
+select * from author;
+select * from book where  isbn like '1234567890123' ;
+select * from authorbook order by bookID;
+select * from publisherbook;
